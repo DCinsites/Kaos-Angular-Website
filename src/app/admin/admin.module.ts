523a440da1b227ac20fda8cad8e17936b4c8cc9a
Darrell Curtright <d.curtright@dcinsites.co>
2018-12-07 12:46:40 -0800

    Added Admin components, working on securing admin rights

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
+import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
+
+@NgModule({
+  declarations: [AdminDashboardComponent, AdminComponent, ManageProductsComponent, ManageUsersComponent, ManageOrdersComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
